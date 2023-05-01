fe0f416992c6b5d326240d085e241b07c7434f1b
alexbs01 <alex.becerra.suarez01@gmail.com>
2023-05-01 13:54:54 +0200

    Sigue pasando lo mismo de que no agrega al array

--- practica02/ej02.ml
+++ practica02/ej02.ml
@@ -20,6 +20,7 @@ funcion activara una excepción *)
 let gic0 = gic_of_string "S A B C;    a b;  S;  S -> A B | B C;   A -> B A| a;     B -> C C | b; C -> A B | a;";;
 
 let cadena0 = cadena_of_string "b b a b";;
+let cadena1 = cadena_of_string "b b a c";;
 
 let rellenamosPrimeraFila matriz cadena =
     let rec aux i j mat cad =
@@ -33,39 +34,47 @@ let rellenamosPrimeraFila matriz cadena =
 let cyk cadena (Gic(_, _, reglas, axioma) as gic) =
     if (cadena = []) || (not (es_fnc gic)) (* Si está vacía o no está en FNC retornamos una excepción *)
     then raise (Failure "La cadena de esta vacia o la gramatica no esta en FNC")
-    else
+    else 
         let n = List.length cadena in (* Longitud de la cadena *)
         (* Creamos una matriz de n * n con conjuntos vacíos *)
-        let matriz = Array.make_matrix n (n + 1) conjunto_vacio in
+        let matriz = Array.make_matrix n n conjunto_vacio in
         let matriz = rellenamosPrimeraFila matriz cadena in (* Rellenamos la primera fila con los símbolos de la cadena *)
         
         let rec iterarJ j matriz =
-            if j > n
-            then pertenece axioma matriz.(1).(n)
-            
-            else
+            if j <= n - 1
+            then 
+
                 let rec iterarI j i matriz =
-                    if i > n - j + 1
-                    then iterarJ (j + 1) matriz
-                    else 
+                    if i <= n - j - 1
+                    then 
+                        
                         let rec iterarK j i k matriz reglas = 
-                            if k <= j - 1
-                                then
+                            if k <= i + j - 1
+                                then 
+                                    
                                     match reglas with
                                         | []  -> iterarI j (i + 1) matriz
                                         | (Regla_gic(a, [b; c]))::tail -> 
-                                            if pertenece b matriz.(i).(k) && pertenece c matriz.(i + k).(j - k)
-                                            then matriz.(i).(j) <- agregar (a) matriz.(i).(j)
-                                            else ();
-                                            
-                                            iterarK j i (k + 1) matriz (list_of_conjunto (suprimir (Regla_gic(a, [b; c])) (conjunto_of_list tail)))
+                                                    if pertenece b matriz.(i).(k) && pertenece c matriz.(i + k).(j - k) 
+                                                    then matriz.(i).(j) <- (agregar a matriz.(i).(j))
+                                                    else ();
 
+                                                    iterarK j i (k + 1) matriz (list_of_conjunto (suprimir (Regla_gic(a, [b; c])) (conjunto_of_list tail)))
+                                        | (Regla_gic(a, [b]))::tail -> 
+                                                    if List.mem b cadena
+                                                    then matriz.(i).(j) <- (agregar a matriz.(i).(j))
+                                                    else ();
+
+                                                    iterarK j i (k + 1) matriz (list_of_conjunto (suprimir (Regla_gic(a, [b])) (conjunto_of_list tail)))
                                         | _::tail -> iterarK j i (k + 1) matriz tail
-                                else iterarI j (i + 1) matriz 
-                        in iterarK j i 1 matriz (list_of_conjunto reglas)
-                in iterarI j 1 matriz
-        in iterarJ 2 matriz;;
-              
+                            else iterarI j (i + 1) matriz 
+                        in iterarK j i i matriz (list_of_conjunto reglas)
 
+                    else iterarJ (j + 1) matriz
+                in iterarI j 0 matriz
+            
+             else(* pertenece axioma matriz.(0).(n - 1)*)matriz
+        in iterarJ 1 matriz;; 
+        
 cyk cadena0 gic0;;
-
+cyk cadena1 gic0;;
